7a3b59445d5c102910479749df9431d8c3fadb05
NayashaPrakash <Nayashap01@gmail.com>
2024-04-04 11:39:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -70,7 +70,7 @@ def ask_question():
     embeddings = OpenAIEmbeddings()
     knowledge_base = FAISS.from_texts(chunks, embeddings)
     docs = knowledge_base.similarity_search(user_question)
-    llm = OpenAI(model_name="text-gpt-3.5-turbo")
+    llm = OpenAI(model_name="gpt-3.5-turbo-instruct")
     chain = load_qa_chain(llm, chain_type="stuff")
     with get_openai_callback() as cb:
         response = chain.run(input_documents=docs, question=user_question)
